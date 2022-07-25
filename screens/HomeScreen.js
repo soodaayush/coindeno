999a283c99030396620bdebb5372399541a3c23c
DevBaddy <warbaddy19@gmail.com>
2022-07-25 16:19:47 -0300

    Completed Firebase authentication integration

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { StyleSheet, View, FlatList, Button, Text, Alert } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
@@ -7,14 +7,63 @@ import "intl/locale-data/jsonp/en";
 
 import TickerItem from "../components/TickerItem";
 import TickerInput from "../components/TickerInput";
+
 import { auth } from "../firebase/config";
 import { useNavigation } from "@react-navigation/core";
+
+import configData from "../config.json";
 
 const HomeScreen = () => {
   const [modalIsVisible, setModalIsVisible] = useState(false);
   const [tickerData, setTickerData] = useState([]);
 
   const navigation = useNavigation();
+
+  useEffect(() => {
+    printData();
+  }, []);
+
+  function printData() {
+    let tickerArray = [];
+
+    let tickers = getTickersFromDatabase();
+
+    tickers.then((data) => {
+      for (let key in data) {
+        let ticker = {
+          key: key,
+          name: data[key].name,
+        };
+
+        tickerArray.push(ticker);
+      }
+
+      let tickersArr = [];
+
+      for (let i = 0; i < tickerArray.length; i++) {
+        let tickerInfo = getTickerData(tickerArray[i].name);
+
+        tickerInfo.then((results) => {
+          if (results.error) {
+            alert(results.error);
+            return;
+          }
+
+          let ticker = {
+            id: results.id,
+            key: tickerArray[i].key,
+            name: results.name,
+            image: results.image.large,
+            price: results.market_data.current_price.cad,
+          };
+
+          tickersArr.push(ticker);
+
+          setTickerData(tickersArr);
+        });
+      }
+    });
+  }
 
   function startModal() {
     setModalIsVisible(true);
@@ -34,17 +83,43 @@ const HomeScreen = () => {
       }
 
       let ticker = {
-        id: results.id,
-        key: Math.random().toString(),
-        name: results.name,
-        image: results.image.large,
-        price: results.market_data.current_price.cad,
+        name: results.id,
       };
 
-      setTickerData([...tickerData, ticker]);
+      saveTickerToDatabase(ticker);
+
+      printData();
     });
 
     endModal();
+  }
+
+  async function saveTickerToDatabase(ticker) {
+    let url = `${configData.BASE_URL}/${auth.currentUser?.uid}/tickers.json`;
+
+    let response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(ticker),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.json();
+  }
+
+  async function getTickersFromDatabase() {
+    try {
+      let url = `${configData.BASE_URL}/${auth.currentUser?.uid}/tickers.json`;
+
+      let response = await fetch(url);
+
+      let responseJson = await response.json();
+
+      return responseJson;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   async function getTickerData(enteredText) {
@@ -63,14 +138,21 @@ const HomeScreen = () => {
     }
   }
 
-  function deleteTicker(id) {
+  async function deleteTickerFromDatabase(key) {
+    let url = `${configData.BASE_URL}/${auth.currentUser?.uid}/tickers/${key}.json`;
+
+    return await fetch(url, {
+      method: "DELETE",
+    });
+  }
+
+  function deleteTicker(key) {
     Alert.alert("Alert", "Are you sure you want to delete this ticker?", [
       {
         text: "Yes",
         onPress: () => {
-          setTickerData((currentTickerData) => {
-            return currentTickerData.filter((ticker) => ticker.id !== id);
-          });
+          deleteTickerFromDatabase(key);
+          printData();
         },
       },
       {
@@ -107,13 +189,13 @@ const HomeScreen = () => {
         <View style={styles.tickersContainer}>
           <FlatList
             data={tickerData}
-            renderItem={(itemData) => {
+            renderItem={(tickerData) => {
               return (
                 <TickerItem
-                  name={itemData.item.name}
-                  price={itemData.item.price}
-                  logo={itemData.item.image}
-                  id={itemData.item.id}
+                  name={tickerData.item.name}
+                  price={tickerData.item.price}
+                  logo={tickerData.item.image}
+                  id={tickerData.item.key}
                   onDeleteTicker={deleteTicker}
                 />
               );
