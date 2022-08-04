5b203ac517890bc8fd2ab55571dd0ef962b0762f
DevBaddy <warbaddy19@gmail.com>
2022-08-04 10:58:27 -0300

    Added Settings page

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import { LogBox } from "react-native";
+
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import AddTickerScreen from "./screens/AddTickerScreen";
+import SettingsScreen from "./screens/SettingsScreen";
 
 const Stack = createNativeStackNavigator();
+
+LogBox.ignoreLogs([
+  "ViewPropTypes will be removed",
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
+]);
 
 export default function App() {
   return (
@@ -25,6 +33,11 @@ export default function App() {
           options={{ headerShown: false }}
           name="Add Ticker"
           component={AddTickerScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Settings"
+          component={SettingsScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
