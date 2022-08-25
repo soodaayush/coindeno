659fc3e4c27a7b9a50c0aae4243b9ea5d9f313cb
DevBaddy <warbaddy19@gmail.com>
2022-08-25 18:09:14 -0300

    Added the theme switching function

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -30,12 +30,14 @@ const HomeScreen = () => {
   const [tickerData, setTickerData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [currency, setCurrency] = useState("");
+  const [theme, setTheme] = useState("");
   const [refresh, setRefresh] = useState(false);
 
   const navigation = useNavigation();
 
   useEffect(() => {
     printData();
+    getTheme();
   }, []);
 
   function printData() {
@@ -44,8 +46,6 @@ const HomeScreen = () => {
     TickerDatabaseService.getInstance()
       .getTickersFromDatabase(auth.currentUser?.uid)
       .then((data) => {
-        setIsLoading(false);
-
         if (data === null) {
           setTickerData([]);
         }
@@ -62,7 +62,7 @@ const HomeScreen = () => {
         let tickersArr = [];
 
         SettingsDatabaseService.getInstance()
-          .getCurrencyFromDatabase(auth.currentUser.uid)
+          .getCurrencyFromDatabase(auth.currentUser?.uid)
           .then((currencyData) => {
             let currency;
 
@@ -70,14 +70,14 @@ const HomeScreen = () => {
               currency = "cad";
               setCurrency(currency);
 
-              let currency = {
+              let currencyObj = {
                 currency: currency,
                 currencyLabel: "CAD - Canadian Dollar",
               };
 
               SettingsDatabaseService.getInstance().saveCurrencyToDatabase(
                 auth.currentUser?.uid,
-                currency
+                currencyObj
               );
             } else {
               for (let key in currencyData) {
@@ -85,6 +85,34 @@ const HomeScreen = () => {
                 setCurrency(currency);
               }
             }
+
+            SettingsDatabaseService.getInstance()
+              .getThemeFromDatabase(auth.currentUser?.uid)
+              .then((themeData) => {
+                setIsLoading(false);
+
+                let theme;
+
+                if (themeData === null) {
+                  theme = "dark";
+                  setTheme(theme);
+
+                  let themeObj = {
+                    theme: theme,
+                    themeLabel: "Dark",
+                  };
+
+                  SettingsDatabaseService.getInstance().saveThemeToDatabase(
+                    auth.currentUser?.uid,
+                    themeObj
+                  );
+                } else {
+                  for (let key in themeData) {
+                    theme = themeData[key].theme;
+                    setTheme(theme);
+                  }
+                }
+              });
 
             for (let i = 0; i < tickerArray.length; i++) {
               TickerDataService.getInstance()
@@ -113,6 +141,8 @@ const HomeScreen = () => {
           });
       });
   }
+
+  function getTheme() {}
 
   function deleteTicker(key) {
     Alert.alert(
@@ -163,31 +193,43 @@ const HomeScreen = () => {
 
   if (isLoading) {
     return (
-      <View style={styles.container}>
-        <StatusBar style="light" />
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          paddingHorizontal: 16,
+          backgroundColor:
+            theme === "dark" ? Colors.backgroundDark : Colors.backgroundLight,
+          paddingTop: 40,
+        }}
+      >
+        <StatusBar style={theme === "light" ? "dark" : "light"} />
         <View style={styles.header}>
           <View style={styles.buttonContainer}>
             <AppButton
-              backgroundColor="#1F4690"
+              backgroundColor={theme === "dark" ? "#1F4690" : ""}
               text="Settings"
-              textColor={Colors.text}
+              textColor={theme === "dark" ? Colors.textDark : "black"}
+              theme={theme}
               onPress={redirectToSettingsPage}
             />
           </View>
           <View style={styles.buttonContainer}>
             <AppButton
-              backgroundColor="#EB1D36"
+              backgroundColor={theme === "dark" ? "#EB1D36" : ""}
               text="Log Out"
-              textColor={Colors.text}
+              textColor={theme === "dark" ? Colors.textDark : "black"}
+              theme={theme}
               onPress={handleSignOut}
             />
           </View>
         </View>
         <View style={styles.addTickerContainer}>
           <AppButton
-            backgroundColor="#377D71"
+            backgroundColor={theme === "dark" ? "#377D71" : ""}
             text="Add Ticker"
-            textColor="#FFE5B4"
+            textColor={theme === "dark" ? Colors.textDark : "black"}
+            theme={theme}
             onPress={redirectToAddTickerPage}
           />
         </View>
@@ -198,31 +240,43 @@ const HomeScreen = () => {
 
   return (
     <>
-      <StatusBar style="light" />
-      <SafeAreaView style={styles.container}>
+      <StatusBar style={theme === "light" ? "dark" : "light"} />
+      <SafeAreaView
+        style={{
+          flex: 1,
+          alignItems: "center",
+          paddingHorizontal: 16,
+          backgroundColor:
+            theme === "dark" ? Colors.backgroundDark : Colors.backgroundLight,
+          paddingTop: 40,
+        }}
+      >
         <View style={styles.header}>
           <View style={styles.buttonContainer}>
             <AppButton
-              backgroundColor="#1F4690"
+              backgroundColor={theme === "dark" ? "#1F4690" : ""}
               text="Settings"
-              textColor={Colors.text}
+              textColor={theme === "dark" ? Colors.textDark : "black"}
+              theme={theme}
               onPress={redirectToSettingsPage}
             />
           </View>
           <View style={styles.buttonContainer}>
             <AppButton
-              backgroundColor="#EB1D36"
+              backgroundColor={theme === "dark" ? "#EB1D36" : ""}
               text="Log Out"
-              textColor={Colors.text}
+              textColor={theme === "dark" ? Colors.textDark : "black"}
+              theme={theme}
               onPress={handleSignOut}
             />
           </View>
         </View>
         <View style={styles.addTickerContainer}>
           <AppButton
-            backgroundColor="#377D71"
+            backgroundColor={theme === "dark" ? "#377D71" : ""}
             text="Add Ticker"
-            textColor="#FFE5B4"
+            textColor={theme === "dark" ? Colors.textDark : "black"}
+            theme={theme}
             onPress={redirectToAddTickerPage}
           />
         </View>
@@ -240,6 +294,7 @@ const HomeScreen = () => {
                   price={tickerData.item.price}
                   logo={tickerData.item.image}
                   id={tickerData.item.key}
+                  theme={theme}
                   currency={currency}
                   onDeleteTicker={deleteTicker}
                 />
@@ -255,13 +310,6 @@ const HomeScreen = () => {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    paddingHorizontal: 16,
-    backgroundColor: Colors.background,
-    paddingTop: 40,
-  },
   header: {
     flexDirection: "row",
     alignItems: "center",
@@ -271,10 +319,6 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     width: 100,
-  },
-  text: {
-    color: Colors.text,
-    fontFamily: "poppins-regular",
   },
   addTickerContainer: {
     width: "90%",
